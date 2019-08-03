c2a9ba61002232647c6cc09034d73a7c9c19e7ff
Paul Beckwith <303dog@gmail.com>
2019-08-03 00:31:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,12 @@ def actor={:name =>"Dwayne The Rock Johnson"}
 
 end
 
-def monopoly
-	monopoly = {:railroads => ""}
+def monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
 
 end
 
